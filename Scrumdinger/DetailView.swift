818ba401a6d578c4bb333cbe22b3095e98a6e477
Kyle Peeler <kpeeler@apple.com>
2023-04-09 12:58:46 -0700

    updates

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Kyle Peeler on 3/19/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,37 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Kyle Peeler on 1/17/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(scrums) { scrum in
+                    NavigationLink(destination: Text(scrum.title)) {
+                        CardView(scrum: scrum)
+                    }.listRowBackground(scrum.theme.mainColor)
+                        
+                }
+            }.navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }.accessibilityLabel("New Scrum")
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
+    }
+}
